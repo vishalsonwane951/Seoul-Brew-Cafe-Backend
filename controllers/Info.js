a1caff05b7245cd3a249df8cce4875f14cfe0b6a
Vishal Sonwane <vishalsonwane951@gmail.com>
2026-02-28 12:12:15 +0530

    -

--- controllers/Info.js
+++ controllers/Info.js
@@ -5,15 +5,15 @@ const router = express.Router();
 router.get("/cafe-info", (req, res) => {
   res.json({
     hours: [
-      { day: "Monday", time: "8 AM – 8 PM" },
-      { day: "Tuesday", time: "8 AM – 8 PM" },
+      { day: "Monday - Friday", time: "9 AM – 10 PM" },
+      { day: "Saturday - Sunday", time: "10 AM – 8 PM" },
       // ...
     ],
     address: {
-      line1: "123 Main St",
-      line2: "Koregaon Park, Pune",
-      phone: "+91 98765 43210",
-      email: "hello@cafe.com"
+      // line1: "123 Main St",
+      line2: "Sihgad Law  College, Pune",
+      phone: "+91 7888251550",
+      email: "goodluck@cafe.com"
     }
   });
 });
